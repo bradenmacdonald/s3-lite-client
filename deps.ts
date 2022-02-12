7d742ddffe491cd3151105148704f24bcfbef033
Braden MacDonald <mail@bradenm.com>
2022-02-12 10:06:47 -0800

    Bump Deno std library version

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { readableStreamFromIterable } from "https://deno.land/std@0.113.0/streams/conversion.ts";
-export { Buffer } from "https://deno.land/std@0.113.0/io/buffer.ts";
+export { readableStreamFromIterable } from "https://deno.land/std@0.125.0/streams/conversion.ts";
+export { Buffer } from "https://deno.land/std@0.125.0/io/buffer.ts";
