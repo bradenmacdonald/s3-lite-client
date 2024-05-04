36f2a1f2432f645c21ed3694da03b50eb0d289a1
Christian Bewernitz <coder@karfau.de>
2024-05-04 20:25:34 +0200

    feat: export S3ClientOptions from mod.ts

--- mod.ts
+++ mod.ts
@@ -3,5 +3,5 @@
  * A lightweight client for connecting to S3-compatible object storage services.
  */
 
-export { Client as S3Client } from "./client.ts";
+export { Client as S3Client, type ClientOptions as S3ClientOptions } from "./client.ts";
 export * as S3Errors from "./errors.ts";
