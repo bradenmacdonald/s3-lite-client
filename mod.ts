be2f6bf0ebc82bb72ac4d0486f0dfd79a392fb9f
Christian Bewernitz <coder@karfau.de>
2024-05-07 07:26:27 +0200

    feat: export all types from client with S3 prefix

--- mod.ts
+++ mod.ts
@@ -3,5 +3,15 @@
  * A lightweight client for connecting to S3-compatible object storage services.
  */
 
-export { Client as S3Client, type ClientOptions as S3ClientOptions } from "./client.ts";
+export {
+  Client as S3Client,
+  type ClientOptions as S3ClientOptions,
+  type CommonPrefix as S3CommonPrefix,
+  type CopiedObjectInfo as S3CopiedObjectInfo,
+  type ObjectMetadata as S3ObjectMetadata,
+  type ObjectStatus as S3ObjectStatus,
+  type ResponseOverrideParams as S3ResponseOverrideParams,
+  type S3Object,
+  type UploadedObjectInfo as S3UploadedObjectInfo,
+} from "./client.ts";
 export * as S3Errors from "./errors.ts";
